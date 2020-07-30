0cc3eea29a067f90f7df91c1ddc30fcd0f99f408
Nguonchhay <nguonchhay.touch@gmail.com>
2020-07-31 00:45:53 +0700

    [TASK] ASPWeb - product list

---
+++ ASPWeb/ASPWeb/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace ASPWeb.Models
+{
+    public class Product
+    {
+
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        [JsonPropertyName("img")]
+        public string Image { get; set; }
+
+        [JsonPropertyName("unit_price")]
+        public float UnitPrice { get; set; }
+
+        public float Discount { get; set; }
+
+        public override string ToString() => JsonSerializer.Serialize<Product>(this);
+    }
+}

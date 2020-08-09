1eae94ac9872c5d4abc67248eda83c048210d629
Nguonchhay <nguonchhay.touch@gmail.com>
2020-08-09 16:52:34 +0700

    [TASK] Udemy - Vidly kickstart

---
+++ ASPWeb/ASPWeb/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASPWeb.Models;
+using ASPWeb.Services;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ASPWeb.Controllers
+{
+    [Route("/api/[controller]")]
+    [ApiController]
+    public class ProductsController : Controller
+    {
+
+        private JsonFileProductService ProductService { get; }
+
+        public ProductsController(JsonFileProductService productService)
+        {
+            ProductService = productService;
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return ProductService.GetProducts();
+        }
+    }
+}
--- Vidly/Vidly/Startup.cs
+++ Vidly/Vidly/Startup.cs
@@ -1,19 +1,15 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text.Json;
 using System.Threading.Tasks;
-using ASPWeb.Models;
-using ASPWeb.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace ASPWeb
+namespace Vidly
 {
     public class Startup
     {
@@ -27,9 +23,7 @@ namespace ASPWeb
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
-            services.AddConnections();
-            services.AddTransient<JsonFileProductService>();
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,11 +35,10 @@ namespace ASPWeb
             }
             else
             {
-                app.UseExceptionHandler("/Error");
+                app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
@@ -55,13 +48,9 @@ namespace ASPWeb
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
-                //endpoints.MapGet("/api/products", (context) =>
-                //{
-                //    var products = app.ApplicationServices.GetService<JsonFileProductService>().GetProducts();
-                //    var json = JsonSerializer.Serialize<IEnumerable<Product>>(products);
-                //    return context.Response.WriteAsync(json);
-                //});
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
